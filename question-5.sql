34b1ee7829945a32a2a9022375b040e7aeea78b6
Kaden Kapadia <106453833+kkapadia10@users.noreply.github.com>
2023-04-11 01:09:05 -0700

    Update to question 5.

--- question-5.sql
+++ question-5.sql
@@ -1,3 +1,9 @@
 -- What was the most purchased track of 2013?
 
-SELECT t.Name AS TrackName, COUNT(il.InvoiceLineId) AS PurchaseCount FROM invoice_items il JOIN tracks t ON il.TrackId = t.TrackId JOIN invoices i ON il.InvoiceId = i.InvoiceId WHERE i.InvoiceDate BETWEEN '2013-01-01 00:00:00' AND '2013-12-31 00:00:00' GROUP BY TrackName ORDER BY PurchaseCount DESC ;
+SELECT t.Name AS TrackName, SUM(il.Quantity) AS PurchaseCount 
+FROM invoice_items il 
+JOIN tracks t ON il.TrackId = t.TrackId 
+JOIN invoices i ON il.InvoiceId = i.InvoiceId 
+WHERE i.InvoiceDate BETWEEN '2013-01-01 00:00:00' AND '2013-12-31 00:00:00' 
+GROUP BY TrackName 
+ORDER BY PurchaseCount DESC;
